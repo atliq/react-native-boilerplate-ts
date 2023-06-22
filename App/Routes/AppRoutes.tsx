62a7d1b4b434d9faaf2bc33bac2af3cabf79ede8
Rutvik Nabhoya <rutvik@atliq.com>
2023-06-22 12:46:25 +0530

    Changed: initial auth routing algorithm

--- App/Routes/AppRoutes.tsx
+++ App/Routes/AppRoutes.tsx
@@ -1,21 +1,12 @@
-import InitialScreen from '@Components/Initial';
 import Login from '@Components/Login/Login';
 import { AppTab } from '@Routes/AppTab';
 
 enum Route {
-  Initial = 'InitialScreen',
   LoginScreen = 'Login',
   HomeScreen = 'Home',
 }
 
 const Routes = [
-  {
-    name: Route.Initial,
-    screen: InitialScreen,
-    navigationOptions: {
-      headerShown: false,
-    },
-  },
   {
     name: Route.LoginScreen,
     screen: Login,
